de9337d59bfa9e41379ab47e569e704fb9056975
ilyafaybisovich <ilya.faybisovich@gmail.com>
2021-09-28 14:06:31 +0200

    Removed unused packages

--- index.test.js
+++ index.test.js
@@ -4,9 +4,3 @@ const { data, output } = require("./sampleData");
 test("evaluates correctly for the initial data", () => {
   expect(evaluateLogFile(data)).toStrictEqual(output);
 });
-
-// test("I can do something with the non-exported", () => {
-//   expect(
-//     evaluateLogFile.__get__("isReferenceReadingsRow")("reference")
-//   ).toStrictEqual(true);
-// });
